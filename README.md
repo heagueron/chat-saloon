0e969609febee727dd255a6e45a5b89887a7e278
unknown <heagueron@gmail.com>
2019-03-05 10:32:57 -0400

    Se agregó listado de mensajes

---
+++ README.md
@@ -0,0 +1,4 @@
+# CHAT Saloon App - Made with node.js and Socket.IO
+
+
+
--- index.js
+++ index.js
@@ -3,19 +3,34 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 
+// Base endpoint:
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 
+// Endpoint for external connection (example spa or desktop app):
+app.get('chat/', function(req, res){
+  // Here, we won´t send an html file.
+  // Just open the connection and let the 
+  // external app manage the ui.
+});
+
+// Socket:
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  
+  // Inform everyone that someone connected
+  io.emit('system message', 'Mensaje del Administrador: Se conectó un usuario');
+  
+  io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+      io.emit('chat message', msg);
+    });
   });
 });
 
 
+// Start server
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
